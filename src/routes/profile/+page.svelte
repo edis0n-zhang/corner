d8fda4b4c9145aed4404b667716c7a3bab75e71c
leozhu0 <leozhu@ucsb.edu>
2023-10-28 10:25:23 -0700

    made progress on profile and pins page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,20 +1,32 @@
 <script>
-
 </script>
 
 <div class="flex justify-center h-screen">
-  <div class="w-1/2">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui. Fusce finibus vel arcu non finibus. Pellentesque vehicula elit at orci pharetra, vel posuere libero volutpat. Proin eu tellus non dolor semper efficitur. Proin sodales, magna a varius convallis, dui turpis facilisis ipsum, id tempus metus dui eu leo. Integer lacinia, justo ut facilisis facilisis, risus elit posuere ipsum, at porttitor lacus mauris sed ligula.
+  <div class="w-1/2 fixed font-monsterrat">
+    <div>
+      
+        <div class="font-semibold mt-20 text-3xl">
+          Username
+        </div>
+
+        <div class="flex flex-row mt-5">
+          <div>
+            123 posts
+          </div>
+          <div class="ml-10 hover:underline">
+            123 followers
+          </div>
+          <div class="ml-10 hover:underline">
+            123 following
+          </div>
+        </div>
+
+        <div class="mt-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Donec in efficitur leo. Maecenas non diam cursus, imperdiet massa eget, pellentesque ex. Vestibulum lacinia porttitor ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed pulvinar egestas libero, in sodales mauris efficitur a. Fusce non bibendum dui.
+        </div>
     </div>
+
+  </div>
 </div>
 
 <style lang="postcss">
